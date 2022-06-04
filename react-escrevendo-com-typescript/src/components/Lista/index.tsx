960912615b8c13e5e64a0bad22c4cc506b69897f
Jonas de Jesus Américo <jjsamerico@gmail.com>
2022-06-03 21:21:06 -0300

    Finalizando aula 3

--- react-escrevendo-com-typescript/src/components/Lista/index.tsx
+++ react-escrevendo-com-typescript/src/components/Lista/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import style from './List.module.scss'
 
 function Lista() {
     const tarefas = [
@@ -16,12 +16,12 @@ function Lista() {
         }
 ]
     return (
-        <aside>
+        <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
             <ul>
                 {
                     tarefas.map((item, index) =>  (
-                        <li key={index}>
+                        <li key={index} className="item">
                             <h3>{item.tarefa}</h3>
                             <span>{item.tempo}</span>
                         </li>
