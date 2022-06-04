f57da897843e061e8c6ba5649ee1ec30d28c5392
Jonas de Jesus Américo <jjsamerico@gmail.com>
2022-06-04 10:40:56 -0300

    Finalizando a aula 5

--- react-escrevendo-com-typescript/src/components/Lista/index.tsx
+++ react-escrevendo-com-typescript/src/components/Lista/index.tsx
@@ -1,28 +1,17 @@
+import { ITarefa } from '../../types/tarefa';
 import Item from './Item';
 import style from './List.module.scss'
 
-function Lista() {
-    const tarefas = [
-        {
-            tarefa: 'React',
-            tempo: '02:00:00'
-        },
-        {
-            tarefa: 'Javascript',
-            tempo: '01:00:00'
-        },
-        {            
-            tarefa: 'Typescript',
-            tempo: '03:00:00'
-        }
-]
+
+function Lista({tarefas}: {tarefas: ITarefa[]}) {
+    
     return (
         <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
             <ul>
                 {
-                    tarefas.map((item, index) =>  (
-                        <Item 
+                    tarefas.map((item, index) => (
+                        <Item
                             key={index}
                             {...item}
                         />
