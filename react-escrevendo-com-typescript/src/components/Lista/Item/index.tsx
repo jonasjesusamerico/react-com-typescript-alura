a5b77e8062c77ac29005d9539b2d879655fe494d
Jonas de Jesus Américo <jjsamerico@gmail.com>
2022-06-04 10:41:27 -0300

    Finalizando a aula 5

--- react-escrevendo-com-typescript/src/components/Lista/Item/index.tsx
+++ react-escrevendo-com-typescript/src/components/Lista/Item/index.tsx
@@ -1,7 +1,8 @@
+import { ITarefa } from '../../../types/tarefa'
 import style from '../List.module.scss'
 
 
-export default function Item( {tarefa, tempo }: {tarefa: string, tempo: string}) {
+export default function Item( {tarefa, tempo, selecionado, completado, id }: ITarefa) {
     return (
         <li className={style.item}>
             <h3>{tarefa}</h3>
